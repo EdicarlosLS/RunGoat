7c20e8e8894b8db06a362503d64a08f63c92480a
EdicarlosLS <jels.23@hotmail.com>
2020-09-01 12:00:24 -0300

    Loop plataformas

--- RunGoat/app/src/main/java/com/edicarlosls/rungoat/jogo/Plataformas.java
+++ RunGoat/app/src/main/java/com/edicarlosls/rungoat/jogo/Plataformas.java
@@ -13,15 +13,21 @@ public class Plataformas
 	public Plataformas(){
 		plataformas = new ArrayList<>();
 		plataformas.addAll(Arrays.asList(
-							   new Plataforma(0, 670, 250, 50)
-							   , new Plataforma(100, 550, 150, 120),
-							   new Plataforma(200, 420, 100, 40)		   
+							   new Plataforma(0, 670, 350, 50),
+							   new Plataforma(100, 550, 150, 120),
+							   new Plataforma(200, 420, 100, 40),
+							   new Plataforma(400, 670, 150, 50),
+							   new Plataforma(600, 670, 250, 50),
+							   new Plataforma(300, 420, 120, 40)	
 						   ));
 	}
 
 	public void atualiza(){
 		for (Plataforma p : plataformas){
-			p.setX(p.getX() - 0.5f);
+			p.setX(p.getX() - 3.5f);
+			if(saiuDaTela(p)){
+				p.setX(1280);
+			}
 		}
 	}
 
@@ -31,5 +37,7 @@ public class Plataformas
 		}
 	}
 
-
+	private boolean saiuDaTela(Plataforma plat){
+		return plat.getX() + plat.getLargura() < 0;
+	}
 }
