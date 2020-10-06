8ddf0db18e49c5893ac8fac717ac56ee1336db57
EdicarlosLS <jels.23@hotmail.com>
2020-10-06 20:02:06 -0300

    fundo bitmap

--- RunGoat/app/src/main/java/com/edicarlosls/rungoat/jogo/Fundo.java
+++ RunGoat/app/src/main/java/com/edicarlosls/rungoat/jogo/Fundo.java
@@ -13,15 +13,13 @@ public class Fundo
 	private float emenda;
 	private float velocidade;
 	private Bitmap img;
-	private Rect rSrc;
-	private Rect rDest;
+	
 	
 	public Fundo(float velocidade, int imgId){
 		this.velocidade = velocidade;
 		emenda = 1280;
-		this.img = BitmapFactory.decodeResource(Jogo.getResources(), imgId);
-		rSrc = new Rect(0, 0, img.getWidth(), img.getHeight());
-		rDest = new Rect(0, 0, 1280, 720);
+		img = BitmapFactory.decodeResource(Jogo.getResources(), imgId);
+		img = Bitmap.createScaledBitmap(img, 1280, 720, false);
 	}
 	
 	public void atualizar(){
@@ -33,12 +31,8 @@ public class Fundo
 	}
 	
 	public void desenhaNo(Canvas canvas){
-		
-		rDest.set((int) emenda - 1280, 0, (int) emenda, 720);
-		canvas.drawBitmap(img, rSrc, rDest, null);
-		
-		rDest.set((int) emenda, 0, (int) emenda + 1280, 720);
-		canvas.drawBitmap(img, rSrc, rDest, null);
+		canvas.drawBitmap(img, emenda - 1280, 0, null);
+		canvas.drawBitmap(img, emenda, 0, null);
 	}
 	
 	public static Fundo f1 = new Fundo(0.7f, R.drawable.nuvem);
